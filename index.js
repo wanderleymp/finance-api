0f664ff53b647cd494232b5f26ae08bfe982e777
wanderleywm <wanderley@agilegestaoempresarial.com.br>
2024-11-14 16:28:47 -0400

    primeiro commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+console.log("Hello, Agile Backend!");
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Agile Backend - Deploy Test!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
