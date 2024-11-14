895b75f4db0705756d9ad5580bcedd99549d7937
wanderleywm <wanderley@agilegestaoempresarial.com.br>
2024-11-14 18:53:44 -0400

    teste de atualizacao

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Agile Backend - Deploy Test!");
+  res.send("Agile Backend API- Deploy Test!");
 });
 
 app.listen(PORT, () => {
