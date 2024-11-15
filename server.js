3fe849e18a0e5064308711ee9789881bb236977c
wanderleywm <wanderley@agilegestaoempresarial.com.br>
2024-11-14 20:03:31 -0400

    adicionando logs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Configuração do banco de dados PostgreSQL
 const pool = new Pool({
   user: 'user_agile', // Substitua pelo seu usuário do PostgreSQL
-  host: '157.90.31.218',   // Ou o IP do seu servidor PostgreSQL
+  host: 'localhost',   // Ou o IP do seu servidor PostgreSQL
   database: 'agile_db', // Substitua pelo nome do seu banco de dados
   password: 'darct99KhVLJXvnbE9eX', // Substitua pela sua senha do PostgreSQL
   port: 5432,           // Porta padrão do PostgreSQL
@@ -20,13 +20,16 @@ app.use(express.json());
 
 // Rota de teste para verificar o funcionamento do servidor
 app.get('/', (req, res) => {
+  console.log('Rota / foi acessada');
   res.send('Hello Agile API!');
 });
 
 // Rota para testar a conexão com o banco de dados
 app.get('/test-db', async (req, res) => {
   try {
+    console.log('Tentando conectar ao banco de dados...');
     await pool.query('SELECT 1');
+    console.log('Conexão com o banco de dados bem-sucedida!');
     res.status(200).send('Conexão com o banco de dados bem-sucedida!');
   } catch (error) {
     console.error('Erro ao conectar ao banco de dados:', error);
@@ -37,6 +40,7 @@ app.get('/test-db', async (req, res) => {
 // Rota para buscar todos os registros de uma tabela específica
 app.get('/dados', async (req, res) => {
   try {
+    console.log('Rota /dados foi acessada');
     const result = await pool.query('SELECT * FROM public.teste'); // Substitua "sua_tabela" pelo nome da sua tabela
     res.status(200).json(result.rows);
   } catch (error) {
@@ -49,9 +53,11 @@ app.get('/dados', async (req, res) => {
 app.post('/dados', async (req, res) => {
   const { campo1, campo2 } = req.body; // Substitua pelos nomes dos campos que deseja inserir
   try {
+    console.log('Tentando inserir um registro na tabela public.teste');
     const query = 'INSERT INTO public.teste (campo1, campo2) VALUES ($1, $2) RETURNING *';
     const values = [campo1, campo2];
     const result = await pool.query(query, values);
+    console.log('Registro inserido com sucesso:', result.rows[0]);
     res.status(201).json(result.rows[0]);
   } catch (error) {
     console.error('Erro ao inserir dados:', error);
@@ -62,4 +68,5 @@ app.post('/dados', async (req, res) => {
 // Iniciando o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Acesse http://localhost:${PORT} ou http://seu-ip-externo:${PORT}`);
 });
