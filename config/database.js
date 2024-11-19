02e1239396235e24e5ae87327dd2f04a4e2e11e4
wanderleymp <wanderley@agilegestaoempresarial.com.br>
2024-11-19 18:45:03 +0000

    funcionando com https

--- config/database.js
+++ config/database.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_CONNECTION_URI, // Usa a string de conexão diretamente
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false, // Configuração SSL opcional
 });
 
 pool.on('connect', () => {
