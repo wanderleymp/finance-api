c764fbeeca508635c42890407d502aac2331133e
wanderleymp <wanderley@agilegestaoempresarial.com.br>
2024-11-25 14:47:08 +0000

    Versão inicial do projeto sincronizada da VPS

--- routes/movementRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getMovementsPaginated } = require('../controllers/movementController');
-
-const router = express.Router();
-
-// Rota para buscar movimentos com paginação
-router.get('/', getMovementsPaginated);
-
-module.exports = router;
